7023059b54f8425f4a2515b9d7b04f66b7b0d0e7
nnirupamapardhu-glitch <nnirupamapardhu@gmail.com>
2026-03-28 18:58:59 +0530

    Create ocr_service.py

---
+++ app/services/ocr_service.py
@@ -0,0 +1,14 @@
+import pytesseract
+from PIL import Image
+import os
+
+# Set the OCR windows path
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def extract_text_from_image(file_path: str) -> str:
+    try:
+        image = Image.open(file_path)
+        text = pytesseract.image_to_string(image)
+        return text
+    except Exception as e:
+        return f"OCR Error: {str(e)}"
